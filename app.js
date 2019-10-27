165f1013185c4d35116feeb0240e1494b855ce9b
Toheeb Adegoke <atoheeb39@gmail.com>
2019-10-27 13:25:04 +0100

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
     scannerInput.focus();
 
     //set some defaut
-    $('.event-year').html(new Date().getFullYear());
+    $('.event-year').html(new Date().getFullYear()+1);
 
     //initialize plugin    
     scannerInput.scannerDetection({
@@ -26,24 +26,47 @@
 
     function fetchUserinfo(barcode){
         let url = `http://eventag.waashow.org/tagData.php?barcode="${barcode}"`;
+        let result = {};
         console.log(url);
-        fetch(url,
-            {
-                method: 'GET', // *GET, POST, PUT, DELETE, etc.
-                mode: 'no-cors', 
-                cache: 'no-cache',
-                credentials: 'same-origin', // include, *same-origin, omit
-        })
+        fetch(url)
         .then((res) => res.json())
-        .then((data)=>{
-            console.log(data);
-    // fname
-    // sname
-    // audience-type
+        .then((dtmessage)=>{
+            if(dtmessage.status == 'SUCCESS' && dtmessage.data){
+                dtmessage.data.forEach(function(item){
+                    result[item.QST_display_text] = item.ANS_value;
+                });
+                $.extend(result, dtmessage.data[0]);
+                //use result
+                console.log(result);
+                $('.fname').html(result.ATT_fname);
+                $('.sname').html(result.ATT_lname);
+                $('.company').html(result['Company']);
+                $('.type').html(result['Participant Type']);
+                $('.sector').html(result.CNT_name);
+                $('.audience-type').html(result['Participant Category']);
 
+            }else{
+                $('.audience-detail span').html("");
+                alert('No record found due to '+dtmessage.message);
+            }
         })
     }
 
-    
+    function printDiv() {
+        var divToPrint=document.querySelector('.audience-detail');
+        var newWin=window.open('','Print-Window');
+        newWin.document.open();
+        newWin.document.write('<html><link rel="stylesheet" href="app.css"><body onload="window.print()">'+divToPrint.outerHTML+'</body></html>');
+        newWin.document.close();
+        setTimeout(function(){newWin.close();},10);
+    }
+
+    $('.clear').on('click',function(){
+        window.location.reload();
+    });
+
+    $('.print').on('click', function(){
+        printDiv();
+    });
 
 }());
